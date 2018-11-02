66c3d7361c1b36180f2f775a7720c77d4cad7662
Rachel Minto <rminto@wistia.com>
2018-11-02 11:55:02 +0100

    Allow for continuous firing.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 1f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
+
+    Coroutine firingCoroutine;
 
     float xMin;
     float xMax;
@@ -16,19 +21,39 @@ public class Player : MonoBehaviour {
         SetupMoveBoundaries();
 	}
 
-    private void SetupMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-    }
 
     // Update is called once per frame
     void Update () {
         Move();
+        Fire();
 	}
+
+    private void Fire()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+           firingCoroutine = StartCoroutine(FireContinuously());
+        }
+
+        if(Input.GetButtonUp("Fire1")) {
+            StopCoroutine(firingCoroutine);
+        }
+    }
+
+    private void CreateLaser() {
+        GameObject laser = Instantiate(
+            laserPrefab,
+            transform.position,
+            Quaternion.identity
+        ) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+    }
+
+    private IEnumerator FireContinuously() {
+        while(true) {
+            CreateLaser();
+            yield return new WaitForSeconds(projectileFiringPeriod);
+        };
+    }
 
     private void Move() {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
@@ -38,5 +63,13 @@ public class Player : MonoBehaviour {
         var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetupMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
