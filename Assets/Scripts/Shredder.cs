a453368d7747b47b5d219fd265f36a7fb444a792
Rachel Minto <rminto@wistia.com>
2018-11-13 11:16:23 +0100

    Clean up lasers when they are off screen.

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Shredder : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("in on trigger enter");
+        Destroy(collision.gameObject);
+    }
+}
