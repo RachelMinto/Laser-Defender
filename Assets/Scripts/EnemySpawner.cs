c57b1e302620eaa2ee14ab9267b7a0b10084cc82
Rachel Minto <rminto@wistia.com>
2018-12-31 09:54:56 -0500

    Spawn all enemy waves.

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
 
     [SerializeField] List<WaveConfig> waveConfigs;
-    int startingWave = 0;
+    [SerializeField] int startingWave = 0;
 
 	// Use this for initialization
 	void Start () {
-        var currentWave = waveConfigs[startingWave];
-
-        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        StartCoroutine(SpawnAllWaves());
 	}
+
+    private IEnumerator SpawnAllWaves() {
+        for (int waveIndex = startingWave; waveIndex < waveConfigs.Count; waveIndex++) {
+            var currentWave = waveConfigs[waveIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+        }
+    }
 
     private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig) {
         for (int i = 0; i < waveConfig.GetNumberOfEnemeies(); i++) {
-            Instantiate(
+            var newEnemy = Instantiate(
                 waveConfig.GetEnemyPrefab(),
                 waveConfig.GetWaypoints()[0].transform.position,
                 Quaternion.identity
             );
+
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
 
             yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
         }
