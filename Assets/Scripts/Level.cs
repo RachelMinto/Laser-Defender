04deb0590d423fcb3281bb1d5d056370e11212a3
Rachel Minto <rminto@wistia.com>
2019-01-07 07:52:43 -0500

    Menu system plus new font.

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Added for scene script!
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
