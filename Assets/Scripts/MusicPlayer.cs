86c5e8a3a566c9da28a55e5d47f22788c50e47b9
Rachel Minto <rminto@wistia.com>
2019-01-09 07:24:21 -0500

    Add music singleton to play across scenes.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        SetUpSingleton();
+	}
+
+    private void SetUpSingleton()
+    {
+        // GetType uses self to get type;
+        if (FindObjectsOfType(GetType()).Length > 1) 
+        {
+            Destroy(gameObject);
+        } 
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
